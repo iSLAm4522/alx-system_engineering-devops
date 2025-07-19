0e0fca2623ae03482c50d04c9e8ab79c88666467
iSLAm4522 <islammadian798@gmail.com>
2025-07-19 12:18:50 +0300

    Add 1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+1-export_to_CSV.py
+
+This module exports employee data to a CSV file with
+all fields enclosed in double quotes.
+
+Usage:
+    ./1-export_to_CSV.py <employee_id>
+"""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+    import csv
+    user_id = sys.argv[1]
+    base_url = 'https://jsonplaceholder.typicode.com'
+    session = requests.Session()
+
+    user_resp = session.get(f"{base_url}/users/{user_id}")
+    user = user_resp.json()
+    employee_name = user.get("name")
+
+    tasks_resp = session.get(f"{base_url}/todos", params={"userId": user_id})
+    tasks = tasks_resp.json()
+
+    with open(f'{user_id}.csv', mode='w', newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for task in tasks:
+            row = [user_id, employee_name, task.get("completed"),
+                   task.get("title")]
+            writer.writerow(row)
