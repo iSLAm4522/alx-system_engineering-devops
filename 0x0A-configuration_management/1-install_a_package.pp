9df63f40e129215f0a5e9301154f9667d62b0f4f
iSLAm4522 <islammadian798@gmail.com>
2025-08-30 13:25:01 +0300

    Fix 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # Puppet manifest to install a specified package on the system.
+package { 'werkzeug':
+    ensure   => '2.1.1',
+    provider => 'pip3'
+}
+
 package { 'flask':
     ensure   => '2.1.0',
-    provider => 'pip3'
+    provider => 'pip3',
+    require  => Package['werkzeug']
 }
